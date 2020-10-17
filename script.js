abae783ab48a6cad980db2096b4016179603305e
Roberto Lopes da Costa <71911175+robert871@users.noreply.github.com>
2020-10-17 20:03:30 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+var texto=document.getElementById(`text`)
+var text=texto.textContent
+
+function pagina(){
+  var foto=  window.document.getElementById(`foto`)
+  foto.src="tela.jpg"
+   window.document.getElementById(`text`).innerText="Mindfull é a atenção plena e a capacidade de focar 100% na atual atividade, sem nenhuma distração. Pode parecer difícil, mas acredite: não é. Assim como em qualquer atividade física, é preciso que treinemos a nossa mente. Por isso, conseguir uma atenção plena é questão de prática e exercícios. Ou seja, ao fazer as atividades com esse objetivo, tenha consciência do seu progresso e não foque apenas nos resultados. Contrariando a crença popular, essa técnica não consiste em esvaziar a mente de pensamentos e de emoções. Muito pelo contrário: ela procura fazer com que o indivíduo preste atenção ao momento presente: o aqui e o agora. O mindfull é, portanto, um treino que compreende conectar o corpo e a mente. Desse modo, ele ajudará você a analisar seus padrões de emoções e de pensamentos, além de se concentrar em suas experiências positivas ou negativas."
+window.document.querySelector("section").style.backgroundColor="#90ff90"
+window.document.querySelector("section").style.color="#ff4000"
+
+}
+
+function pagina1(){
+  var foto=  window.document.getElementById(`foto`)
+  foto.src="robo.jpg"
+  window.document.getElementById(`text`).innerHTML=text
+window.document.querySelector("section").style.backgroundColor="#8585ff"
+window.document.querySelector("section").style.color="black"
+
+}
+function pagina3(){
+  var foto=  window.document.getElementById(`foto`)
+  foto.src="img.jpg"
+  window.document.getElementById(`text`).innerHTML="Como praticar o mindfull? Para que você entenda como começar a praticar essa técnica, abordei algumas dicas para facilitar o seu dia a dia. Observe sua respiração Sentado ou de pé em uma posição confortável e em um local onde não haja barulhos, respire naturalmente. Relaxe, focando nas sensações de seu estômago, suas narinas e seu peito. Caso a mente vague, traga a atenção de volta à respiração. Foque em algo no dia a dia O próximo passo para se aprimorar nas técnicas do mindfull é tentar levar a consciência do aqui e agora para todas as atividades do seu dia. Exemplos práticos podem ser levados em consideração, como reparar na temperatura da água do banho, no movimento de suas mãos enquanto executa alguma atividade, em sua linguagem corporal durante o trabalho, entre outros."
+window.document.querySelector("section").style.backgroundColor="#ff9090"
+window.document.querySelector("section").style.color="black"
+
+}
